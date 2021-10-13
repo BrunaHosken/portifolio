77ea9d5f6be4d2a0923c8c6209596ed34353362d
Bruna Hosken <bruna16.hosken@gmail.com>
2021-10-12 22:39:14 -0300

    :package: footer e :package: pre-load

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+var i = setInterval(function () {
+    
+    clearInterval(i);
+  
+    // O código desejado é apenas isto:
+    document.getElementById("loading-wrapper").style.display = "none";
+    document.getElementById("container").style.display = "inline";
+
+}, 4000);
